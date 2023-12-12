6922313a8fb2b9d05afbc0272f802dfecf7a016a
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-12 15:37:50 -0500

    initial bundle logic

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 import React, { useState, useEffect, useRef } from "react";
 import ReactDom from "react-dom";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 
 const App = () => {
   const ref = useRef<any>();
@@ -22,13 +23,15 @@ const App = () => {
     if (!ref.current) {
       return;
     }
-
-    const result = await ref.current.transform(input, {
-      loader: "jsx",
-      target: "es2015",
+    const result = await ref.current.build({
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin()],
     });
 
-    setCode(result.code);
+    // console.log("result: ", result);
+    setCode(result.outputFiles[0].text);
   };
 
   return (
