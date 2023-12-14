c60d9bf6c998dee123852e8c9616fe8913c01cca
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-14 14:26:53 -0500

    onResolve basic implementation

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -5,8 +5,15 @@ export const unpkgPathPlugin = () => {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResole", args);
-        return { path: args.path, namespace: "a" };
+        console.log("onResolve", args);
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
+            namespace: "a",
+          };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
