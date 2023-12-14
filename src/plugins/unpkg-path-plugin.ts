ea68f09523d762ec6daa11900b5ee0e36f111eca
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-14 16:42:21 -0500

    URL solution

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,12 +10,14 @@ export const unpkgPathPlugin = () => {
         if (args.path === "index.js") {
           return { path: args.path, namespace: "a" };
         }
-        //  else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js",
-        //     namespace: "a",
-        //   };
-        // }
+
+        if (args.path.includes("./") || args.path.includes("../")) {
+          return {
+            path: new URL(args.path, args.importer + "/").href,
+            namespace: "a",
+          };
+        }
+
         return { path: `https://unpkg.com/${args.path}`, namespace: "a" };
       });
 
@@ -26,7 +28,7 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('tiny-test-pkg');
+              const message = require('medium-test-pkg');
               console.log(message);
             `,
           };
