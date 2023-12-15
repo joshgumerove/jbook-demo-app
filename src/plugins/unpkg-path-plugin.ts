e574b1b7bfbfd88c55fe310d31f782f766231546
Josh Gumerove <jgumerove@kaufmanrossin.com>
2023-12-15 13:34:48 -0500

    resolve nested paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -13,7 +13,10 @@ export const unpkgPathPlugin = () => {
 
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
-            path: new URL(args.path, args.importer + "/").href,
+            path: new URL(
+              args.path,
+              "https://unpkg.com" + args.resolveDir + "/"
+            ).href,
             namespace: "a",
           };
         }
@@ -35,8 +38,12 @@ export const unpkgPathPlugin = () => {
         }
 
         const { data, request } = await axios.get(args.path);
-        console.log("request: ", request);
-        return { loader: "jsx", contents: data };
+
+        return {
+          loader: "jsx",
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname,
+        };
       });
     },
   };
