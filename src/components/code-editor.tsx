5a0811799f0b378f52ed25af12364fe5211f161d
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-01-27 20:24:50 -0500

    directly configure editor

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -2,7 +2,14 @@ import MonacoEditor from '@monaco-editor/react'
 
 const CodeEditor = () => {
     return <MonacoEditor height="500px" language="javascript" theme='dark' options={{
-        wordWrap: "on"
+        wordWrap: "on",
+        minimap: {enabled: false},
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true
     }}/>
 }
 
