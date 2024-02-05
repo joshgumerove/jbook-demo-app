32ec592a35194b5aa28fc9649316e2827245b7f8
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-02-04 21:04:06 -0500

    add formatting via classNames to button

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -39,7 +39,12 @@ const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
 
   return (
     <div>
-      <button onClick={onFormatClick}>Format</button>
+      <button
+        onClick={onFormatClick}
+        className="button button-format is-primary is-small"
+      >
+        Format
+      </button>
       <MonacoEditor
         editorDidMount={onEditorDidMount}
         value={initialValue}
