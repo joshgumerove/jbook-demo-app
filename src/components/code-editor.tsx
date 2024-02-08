183a8cf07c4dfa292ae97bde8d0317dd7a6c1c08
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-02-08 16:48:45 -0500

    add css styling for syntax highlights of jsx

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,4 +1,5 @@
 import "./code-editor.css";
+import "./syntax.css";
 import React, { useRef } from "react";
 import MonacoEditor, { EditorDidMount } from "@monaco-editor/react";
 import prettier from "prettier";
