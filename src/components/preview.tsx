bf689e5b84563bff8fa29fce4d55e632b7886d76
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-02-10 11:55:35 -0500

    extract preview component

---
+++ src/components/preview.tsx
@@ -0,0 +1,45 @@
+import React, {useRef, useEffect} from "react"
+
+interface PreviewProps {
+    code: string
+}
+
+const html = `
+<html>
+ <head>
+   <body>
+   <div id="root"></div>
+   <script>
+     window.addEventListener('message', (event) => {
+       try {
+         eval(event.data)
+       } catch(err){
+         const root = document.querySelector('#root');
+         root.innerHTML = '<div style="color: red; text-align: center;"><h4>Runtime Error</h4>' + err + '</div>'
+         throw err;
+       }
+     }, false)
+   </script>
+   </body>
+ </head>
+</html>
+ `;
+
+
+const Preview: React.FC<PreviewProps> = ({code}) => {
+    const iframe = useRef<any>();
+
+    useEffect(() => {
+        iframe.current.srcdoc = html
+        iframe.current.contentWindow.postMessage(code, "*");
+    }, [code])
+
+    return <iframe   
+      ref={iframe}
+      title="preview"
+      sandbox="allow-scripts"
+      srcDoc={html}
+    />
+}
+
+export default Preview
--- src/index.tsx
+++ src/index.tsx
@@ -5,10 +5,11 @@ import ReactDom from "react-dom";
 import { unpkgPathPlugin } from "./plugins/unpkg-path-plugin";
 import { fetchPlugin } from "./plugins/fetch-plugin";
 import CodeEditor from "./components/code-editor";
+import Preview from './components/preview'
 
 const App = () => {
   const ref = useRef<any>();
-  const iframe = useRef<any>();
+  const [code, setCode] = useState('')
   const [input, setInput] = useState("");
 
   const startService = async () => {
@@ -27,8 +28,6 @@ const App = () => {
       return;
     }
 
-    iframe.current.srcdoc = html;
-
     const result = await ref.current.build({
       entryPoints: ["index.js"],
       bundle: true,
@@ -40,29 +39,8 @@ const App = () => {
       },
     });
 
-    iframe.current.contentWindow.postMessage(result.outputFiles[0].text, "*");
+    setCode(result.outputFiles[0].text);
   };
-
-  const html = `
- <html>
-  <head>
-    <body>
-    <div id="root"></div>
-    <script>
-      window.addEventListener('message', (event) => {
-        try {
-          eval(event.data)
-        } catch(err){
-          const root = document.querySelector('#root');
-          root.innerHTML = '<div style="color: red; text-align: center;"><h4>Runtime Error</h4>' + err + '</div>'
-          throw err;
-        }
-      }, false)
-    </script>
-    </body>
-  </head>
- </html>
-  `;
 
   return (
     <div>
@@ -77,12 +55,7 @@ const App = () => {
       <div>
         <button onClick={onClick}>Submit</button>
       </div>
-      <iframe
-        ref={iframe}
-        srcDoc={html}
-        title="preview"
-        sandbox="allow-scripts"
-      ></iframe>
+      <Preview code={code}/>
     </div>
   );
 };
