65fa63fcf3e52f2b906ef04d5854f0a997946cae
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-02-26 23:43:28 -0500

    create horizontal resizer

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -12,21 +12,23 @@ const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
   let resizableProps: ResizableBoxProps;
 
   if (direction === "horizontal") {
-    resizableProps = {};
+    resizableProps = {
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ["e"],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+    };
   } else {
-    resizableProps = {};
+    resizableProps = {
+      height: 300,
+      width: Infinity,
+      resizeHandles: ["s"],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      minConstraints: [Infinity, 24],
+    };
   }
-  return (
-    <ResizableBox
-      height={300}
-      width={Infinity}
-      resizeHandles={["s"]}
-      maxConstraints={[Infinity, window.innerHeight * 0.9]}
-      minConstraints={[Infinity, 24]}
-    >
-      {children}
-    </ResizableBox>
-  );
+  return <ResizableBox {...resizableProps}>{children}</ResizableBox>;
 };
 
 export default Resizable;
