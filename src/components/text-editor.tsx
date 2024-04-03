4565b6954975363fe7650596ab26329c2eae2694
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-02 21:25:22 -0400

    add event listener to document

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,8 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
   const [editing, setEditing] = useState(false);
+  useEffect(() => {
+    const listener = () => {
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
 
   if (editing) {
     return (
