033c5bf6de503d1963ca88ca33d096f7ede18e45
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-02 21:18:09 -0400

    add event listener to TextEditor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import MDEditor from "@uiw/react-md-editor";
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false);
+
+  if (editing) {
+    return (
+      <div>
+        <MDEditor />
+      </div>
+    );
+  }
   return (
-    <div>
-      <MDEditor />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={"# Header"} />
     </div>
   );
 };
