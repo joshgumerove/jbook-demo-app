bdf1ebd806aac027cb39b24f1dbb31170eae686f
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-11 15:58:19 -0400

    text editor bar styling

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -20,7 +20,7 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
+      <div className="text-editor" ref={ref}>
         <MDEditor />
       </div>
     );
