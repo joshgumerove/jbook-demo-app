8736b41a7530a117dd7b62022c9406af68c662f5
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-04-24 21:18:58 -0400

    create action interfaces

--- src/state/actions/index.ts
+++ src/state/actions/index.ts
@@ -1,1 +1,24 @@
-export {};
+import { ActionType } from "../action-types";
+
+interface MoveCellAction {
+  type: ActionType.MOVE_CELL;
+}
+
+interface DeleteCellAction {
+  type: ActionType.DELETE_CELL;
+}
+
+interface InsertCellBeforeActoin {
+  type: ActionType.INSERT_CELL_BEFORE;
+}
+
+interface UpdateCellAction {
+  type: ActionType.UPDATE_CELL;
+}
+
+export type {
+  MoveCellAction,
+  DeleteCellAction,
+  InsertCellBeforeActoin,
+  UpdateCellAction,
+};
