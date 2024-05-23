30c067723080379573ecad3eeb2aaa684d5ccc67
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-23 10:28:53 -0400

    return state in reducer

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -24,12 +24,12 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload;
       state.data[id].content = content;
-      return;
+      return state;
 
     case ActionType.DELETE_CELL:
       delete state.data[action.payload];
       state.order = state.order.filter((id) => id !== action.payload);
-      return;
+      return state;
 
     case ActionType.INSERT_CELL_BEFORE:
       const cell: Cell = {
@@ -49,7 +49,7 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       } else {
         state.order.splice(foundIndex, 0, cell.id);
       }
-      return;
+      return state;
 
     case ActionType.MOVE_CELL:
       const { direction } = action.payload;
@@ -57,12 +57,12 @@ const reducer = produce((state: CellsState = initialState, action: Action) => {
       const targetIndex = direction === "up" ? index - 1 : index + 1;
 
       if (targetIndex < 0 || targetIndex > state.order.length - 1) {
-        return;
+        return state;
       }
 
       state.order[index] = state.order[targetIndex];
       state.order[targetIndex] = action.payload.id;
-      return;
+      return state;
 
     default:
       return state;
