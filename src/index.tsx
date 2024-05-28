4946a3478f50c846e16dbf862533bdc5eab1fda1
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-28 11:06:33 -0400

    refactor

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 import { Provider } from "react-redux";
 import { store } from "./state";
+import CellList from "./components/cell-list";
 import ReactDom from "react-dom";
-import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
     <Provider store={store}>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   );
