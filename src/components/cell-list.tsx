7343d0dd98c3f79b155daf618c3d413f395811b9
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-05-30 11:15:52 -0400

    useTypedSelector

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 
 const CellList: React.FC = () => {
-  useTypedSelector((state) => state);
+  useTypedSelector(({ cells: { order, data } }) => {
+    return order.map((id) => {
+      return data[id];
+    });
+  });
   return <div>Cell List</div>;
 };
 
