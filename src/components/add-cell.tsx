b4aa6877aadcc1a6f41ee40e7f2b0f064ca64542
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-07-17 16:49:43 -0400

    styling

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -11,11 +11,23 @@ const AddCell: React.FC<AddCellProps> = ({ nextCellid }) => {
   return (
     <div className="add-cell">
       <div className="add-buttons">
-        <button onClick={() => insertCellBefore(nextCellid, "code")}>
-          Code
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellid, "code")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Code</span>
         </button>
-        <button onClick={() => insertCellBefore(nextCellid, "text")}>
-          Text
+        <button
+          className="button is-rounded is-primary is-small"
+          onClick={() => insertCellBefore(nextCellid, "text")}
+        >
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Text</span>
         </button>
       </div>
       <div className="divider"></div>
