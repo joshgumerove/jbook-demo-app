63df2ce78a74bf0b9a848ec3726232ba95bb54e4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-07-24 14:38:34 -0400

    insert cell refactor

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -13,16 +13,16 @@ const CellList: React.FC = () => {
   const renderedCells = cells.map((cell) => {
     return (
       <Fragment key={cell.id}>
-        <AddCell nextCellid={cell.id} />
         <CellListItem cell={cell} />
+        <AddCell previousCellId={cell.id} />
       </Fragment>
     );
   });
 
   return (
     <div>
+      <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
-      <AddCell forceVisible={cells.length === 0} nextCellid={null} />
     </div>
   );
 };
