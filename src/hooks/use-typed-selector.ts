906b30c3052bc2171ba51ecbbd789dcb7f0e3ad4
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-07-29 20:36:20 -0400

    cumulative code calculation

--- src/hooks/use-typed-selector.ts
+++ src/hooks/use-typed-selector.ts
@@ -1,4 +1,4 @@
-import { UseSelector, TypedUseSelectorHook } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux";
 import { RootState } from "../state";
 import { useSelector } from "react-redux";
 
