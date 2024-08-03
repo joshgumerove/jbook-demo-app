1d4ea9da52cd508fc5238e54b431b0311641d305
Josh Gumerove <jgumerove@kaufmanrossin.com>
2024-08-03 00:26:23 -0400

    styling

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,6 +2,7 @@ import React, { Fragment } from "react";
 import { useTypedSelector } from "../hooks/use-typed-selector";
 import AddCell from "./add-cell";
 import CellListItem from "./cell-list-item";
+import "./cell-list.css";
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => {
@@ -20,7 +21,7 @@ const CellList: React.FC = () => {
   });
 
   return (
-    <div>
+    <div className="cell-list">
       <AddCell forceVisible={cells.length === 0} previousCellId={null} />
       {renderedCells}
     </div>
